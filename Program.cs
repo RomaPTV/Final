59c26b840274e03bbd7e16edaafa70061ebbcf0e
Roman <poteev.roman@mail.ru>
2023-02-06 22:29:25 +0300

    Final

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] Citys = new string[7] {"Москва", "Спб", "Пермь", "Екатеринбург", "Уфа", "Реж", "Владимир"};
+string[] kolvo = new string[Citys.Length];
+
+int count = 0;
+
+for (int i = 0; i < Citys.Length; i++)
+{
+    if (Citys[i].Length <= 3 )
+        {
+            kolvo[count] = Citys[i];
+            count++;
+            Console.WriteLine(Citys[i]);
+        }
+}
+
